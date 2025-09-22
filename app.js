8fc551688f8f5fd040182a02201b38161af92a00
Medrano Jerónimo <medrano.jeronimo.12@gmail.com>
2025-09-22 18:11:51 -0300

    Versión final

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function asignarElementoTexto(elemento, texto) {
     elementoHTML.innerHTML = texto;
 }
 
+//función agregar amigo a la lista
+// Mostrar la lista final
 function agregarAmigo(){
     let nombresAñadidos = document.getElementById("amigo").value;
     if (nombresAñadidos==""){
@@ -19,18 +21,31 @@ function agregarAmigo(){
     }
 }
 
+//Función limpiar caja de texto
 function limpiarCampo(){
     let valorCaja= document.getElementById("amigo");
     valorCaja.value="";
 }
 
-
+// Función para mostrar la lista de amigos con <li>
 function mostrarLista() {
     let lista = document.getElementById("listaAmigos");
     // limpiar la lista antes de mostrar
     lista.innerHTML = ""; 
-
+    //recorre la lista
     for (let posición = 0; posición < amigos.length; posición++) {
         lista.innerHTML =lista.innerHTML + `<li>${amigos[posición]}</li>`;
     }
+}
+
+//Función sorteo aleatorio
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Agregue al menos un amigo.");
+    }
+//selecciona una posición de la lista
+    let posiciónAleatoria = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[posiciónAleatoria];
+
+    asignarElementoTexto("resultado", `El amigo secreto es: ${amigoSecreto}`);
 }
