cb40e223b2c7e6f9bfe9712bb3545b70e4cdd816
Medrano Jerónimo <medrano.jeronimo.12@gmail.com>
2025-09-22 17:30:53 -0300

    lista y funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos =[];
+
+function asignarElementoTexto(elemento, texto) {
+    let elementoHTML= document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo(){
+    let nombresAñadidos = document.getElementById("amigo").value;
+    if (nombresAñadidos==""){
+        alert("Por favor, inserte un nombre");
+    } else {
+    amigos.push(nombresAñadidos);
+    limpiarCampo();
+    asignarElementoTexto ("listaAmigos", `${amigos}`);
+    }
+}
+
+function limpiarCampo(){
+    let valorCaja= document.getElementById("amigo");
+    valorCaja.value="";
+}
+
